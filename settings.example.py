8c88f43caae0f08c146114074408961135235fc4
rueckix <39458989+rueckix@users.noreply.github.com>
2023-12-24 11:55:54 +0100

    updated settings example

--- settings.example.py
+++ settings.example.py
@@ -1,8 +1,25 @@
+# backends to be enabled
 BACKENDS = [
     'gigaset.backends.local',
-    'gigaset.backends.dastelefonbuch',
+#    'gigaset.backends.dastelefonbuch',
     'gigaset.backends.dasoertliche',
 ]
+
+# own countrycode
 COUNTRY = 'DE'
+
+# own area code
 AREACODE = '030'
+
+# enable/disable debug messages
 DEBUG = False
+
+# port for listening network socket
+PORT=12345
+
+# MQTT enable flag
+MQTT = True
+MQTT_HOST = ""
+MQTT_PORT = 123
+MQTT_USER = ""
+MQTT_PASS = ""
